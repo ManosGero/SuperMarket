4f7501a3f6a1e41eb7c76e3b0867163eca46bef2
Manos Gerothanasis <pli1723@gmail.com>
2021-08-27 21:07:02 +0300

    SuperMarket This Java project use maven. It has a Mariadb database named sm Connector class is working
    add Login Form

---
+++ src/main/java/Users.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Users {
+    private JPanel panel1;
+    private JTable tableUsers;
+    private JButton updateButton;
+    private JButton deleteButton;
+    private JButton insertButton;
+    private JButton button4;
+    private JTextField textFieldName;
+    private JPasswordField sadPasswordField;
+    private JComboBox comboBoxManager;
+    private JComboBox comboBoxTitle;
+    private JTextField textFieldSName;
+    private JTextField textFieldEmail;
+}
